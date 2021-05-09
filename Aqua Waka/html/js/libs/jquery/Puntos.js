a049e1789ba187d6caf6583aecfebbdc008a8d9f
FernandoDGU <66134044+FernandoDGU@users.noreply.github.com>
2021-05-08 19:31:18 -0500

    Updates Varios

---
+++ Aqua Waka/html/js/libs/jquery/Puntos.js
@@ -0,0 +1,11 @@
+var puntos = "";
+
+function SetPuntos(verpuntaje){
+    puntos = verpuntaje;
+}
+
+function getPuntos (){
+    return puntos;
+}
+// muestra=window.setInterval(function(){
+// document.getElementById("pts").innerHTML = MI_VARIABLE_GLOBAL;});
